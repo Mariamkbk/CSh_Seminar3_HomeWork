1f31969145b58893fd73aa551f20b35af47712e1
Maria <kuzmenkomkbk@gmail.com>
2022-11-28 15:22:15 +0300

    Task 10 was edited

--- CShSem2HW/Sem2_10/Program.cs
+++ CShSem2HW/Sem2_10/Program.cs
@@ -4,21 +4,24 @@
 // 782 -> 8
 // 918 -> 1
 
-Console.WriteLine("Введите трехзначное число");
-int a = Convert.ToInt32(Console.ReadLine());
-
-if (a > 99 && a < 1000)
+try
 {
-    int res = (a % 100) / 10;
-    Console.WriteLine($"{res} является второй цифрой числа {a}");
+    Console.WriteLine("Введите трёхзначное число");
+    int a = Convert.ToInt32(Console.ReadLine());
+    if (a > 99 && a < 1000)
+    {
+        int res = (a % 100) / 10;
+        Console.WriteLine($"{res} является второй цифрой числа {a}");
+    }
+    else if (a < 0)
+    {
+        a = a * (-1);
+        int res = (a % 100) / 10;
+        Console.WriteLine($"{res} является второй цифрой числа -{a}");
+    }
+    else Console.WriteLine("Введенное Вами число не является трёхзначным");
 }
-else if (a < 0)
+catch 
 {
-    a = a * (-1);
-    int res = (a % 100) / 10;
-    Console.WriteLine($"{res} является второй цифрой числа -{a}");
-}
-else
-{
-    Console.WriteLine("Введено некорректное число");
+    Console.WriteLine("Введены некорректные символы. Пожалуйста, вводите только числовое значение.");
 }
