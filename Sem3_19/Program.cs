379c8868fb51c1cdf9f366510c86458e76dbd501
Maria <kuzmenkomkbk@gmail.com>
2022-11-29 17:00:12 +0300

    finished task 19

--- Sem3_19/Program.cs
+++ Sem3_19/Program.cs
@@ -4,3 +4,39 @@
 // 12821 -> да
 // 23432 -> да
 
+try
+{
+    Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 9999 && num < 100000 || num < -9999 && num > -100000)
+{
+    int FirstNum = num / 10000;
+    int LastNum = num % 10;
+    if (FirstNum == LastNum)
+    {
+        FirstNum = (num / 1000) % 10;
+        LastNum = (num / 10) % 10;
+        if (FirstNum == LastNum)
+        {
+            Console.WriteLine($"Число {num} является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine($"Число {num} не является палиндромом");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Число {num} не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine($"Число {num} не является пятизначным");
+}
+}
+catch
+{
+    Console.WriteLine($"Некорректные данные. Убедитесь, что вводите числовое значение.");
+}
+
