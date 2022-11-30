0696e2897004c205863fa411a2e650bbcaa375ee
Maria <kuzmenkomkbk@gmail.com>
2022-11-30 11:35:36 +0300

    Task 21 fixed

--- Sem3_21/Program.cs
+++ Sem3_21/Program.cs
@@ -7,18 +7,23 @@ double FindDistance (double x1, double x2, double y1, double y2, double z1, doub
     double distance = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
     return distance;
 }
+try
 {
-Console.WriteLine("Введите координаты x1: ");    
+Console.WriteLine("Введите координаты точки А на оси x");    
 double x1 = Convert.ToDouble(Console.ReadLine());    
-Console.WriteLine("Введите координаты x2: ");    
+Console.WriteLine("Введите координаты точки B на оси x");    
 double x2 = Convert.ToDouble(Console.ReadLine());    
-Console.WriteLine("Введите координаты y1: ");    
+Console.WriteLine("Введите координаты точки А на оси y");    
 double y1 = Convert.ToDouble(Console.ReadLine()); 
-Console.WriteLine("Введите координаты y2: ");    
+Console.WriteLine("Введите координаты точки B на оси y");    
 double y2 = Convert.ToDouble(Console.ReadLine());    
-Console.WriteLine("Введите координаты z1: ");    
+Console.WriteLine("Введите координаты точки А на оси z");    
 double z1 = Convert.ToDouble(Console.ReadLine());  
-Console.WriteLine("Введите координаты z2: ");    
+Console.WriteLine("Введите координаты точки B на оси z");    
 double z2 = Convert.ToDouble(Console.ReadLine()); 
 Console.WriteLine(FindDistance (x1,x2,y1,y2,z1,z2)); 
+}
+catch
+{
+   Console.WriteLine("Введены некорректные данные. Убедитесь, что ввели числовое значение");   
 }
