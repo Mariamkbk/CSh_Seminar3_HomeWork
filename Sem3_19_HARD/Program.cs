eab43f14ff8867a6bebe29ecd362ffb3f5058b24
Maria <kuzmenkomkbk@gmail.com>
2022-11-30 14:21:28 +0300

    Task 19 in progress

--- Sem3_19_HARD/Program.cs
+++ Sem3_19_HARD/Program.cs
@@ -1,13 +1,28 @@
 ﻿// Напишите программу, которая принимает на вход целое число любой разрядности и проверяет, 
 // является ли оно палиндромом. Через строку нельзя решать само собой.
 
+int mult = DigNum;
+
 try
 {
-Console.WriteLine("Введите число");
-int num = Convert.ToInt32(Console.ReadLine());
-int DigNum= Convert.ToInt32(Math.Log10(num));
+    Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int DigNum = Convert.ToInt32(Math.Log10(num));
+    int FirstNum = (Convert.ToInt32(num / (Math.Pow(10, (mult))))) % 10;
+    int LastNum = num % 10;
+    while (mult > (DigNum / 2))
+        if (FirstNum == LastNum)
+        {
+            mult--;
+            FirstNum = (Convert.ToInt32(num / (Math.Pow(10, (mult))))) % 10;
+            LastNum = num % 10 * count;
+        }
+        else
+        {
+            Console.WriteLine($"Число {num} не является палиндромом");
+        }
 }
 catch
 {
-Console.WriteLine("Введены некорректные данные. Пожалуйста, вводите только числовое значение");
+    Console.WriteLine("Введены некорректные данные. Пожалуйста, вводите только числовое значение");
 }
